78a02bfa9405981bbdc95409937f6702e0d6ac3d
laliMaryLeticinha231dsnFabisDil <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2023-08-04 11:50:16 -0300

    feito

---
+++ aula.js
@@ -0,0 +1,16 @@
+// gera números aleatórios
+console.log(Match.random());
+console.log(Match.random() *20);
+console.log (Match.random(7 - 3 + 1)-3);
+
+// arredonda o valor inteiro mais próximo
+console.log(Match.round(3.1));
+console.log (Match.round(3.9));
+
+// arredonda para Baixo
+console.log(Match.floor(3.1));
+console.log(Match.floor(3.9));
+
+// arredonda para cima
+console.log(Match.ceil(3.1));
+console.log(Match.ceil(3.9));
